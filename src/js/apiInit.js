ef472534aebb81f535ccb67eeb05d1f5bafd82bb
x-at-01 <x-at-01@googlegroups.com>
2026-08-20 14:13:56 +0800

    .

--- src/js/apiInit.js
+++ src/js/apiInit.js
@@ -1,7 +1,13 @@
-import { setApi, setOnCaptcha, setOnErr } from "@1-/protoapi";
+import { setApi, setFetch, setOnCaptcha, setOnErr } from "@1-/protoapi";
+import { langCode } from "./i18n.js";
 import { toastErr } from "./Toast.js";
 
 setApi(API);
+
+setFetch((url, conf) => {
+  conf.headers["accept-language"] = langCode();
+  return fetch(url, conf);
+});
 
 setOnCaptcha(() => {
   console.log("todo setOnCaptcha");
